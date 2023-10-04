562859dc956c82c922871ecee7f66886b3b47880
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-10-04 12:30:33 -0500

    Update model-reviews-by-book.php

--- model-reviews-by-book.php
+++ model-reviews-by-book.php
@@ -2,7 +2,7 @@
 function selectReviewsbyBook($bid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT reviews_id, review_title, rating_id FROM `reviews`c join books b on b.book_id = c.book_id where b.book_id=?");
+        $stmt = $conn->prepare("SELECT book_id, book_title, book_author, rating_id FROM `reviews`c join books b on b.book_id = c.book_id where b.book_id=?");
       $stmt->bind_param(b",$bid);
         $stmt->execute();
         $result = $stmt->get_result();

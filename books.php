2675f3096e6504921a3382516ef2d70f2f3a78dd
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-10-04 11:26:08 -0500

    Update books.php

--- books.php
+++ books.php
@@ -4,6 +4,6 @@ require_once("model-books.php");
 $pageTitle ="Books";
 include "view-header.php";
 $books = selectBooks();
-include "view-instructors.php";
+include "view-books.php";
 include "view-footer.php";
 ?>

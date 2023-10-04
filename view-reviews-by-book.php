f6c36a38a94ea5f4beec62c030c31e17f40df418
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-10-04 12:35:03 -0500

    Update view-reviews-by-book.php

--- view-reviews-by-book.php
+++ view-reviews-by-book.php
@@ -1,10 +1,11 @@
-<h1>Reviews</h1>
+<h1>Reviews by Book</h1>
 <div class="table-responsive">
   <table class="table">
     <thead>
       <tr>
       <th>ID</th>
       <th>Title</th>
+      <th>Author</th>
       <th>Rating</th>
         </tr>
     </thead>
@@ -14,7 +15,8 @@ while($review= $reviews->fetch_assoc()){
   ?>
   <tr>
     <td><?php echo $review['reviews_id']; ?></td>
-    <td><?php echo $review['review_title']; ?></td>
+    <td><?php echo $review['book_title']; ?></td>
+    <td><?php echo $review['book_author']; ?></td>
     <td><?php echo $review['rating_id']; ?></td>
   </tr>
   <?php
